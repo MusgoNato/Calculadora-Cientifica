56524ecb325f97b8b812186e505236a05cded1e6
Hugo Josué lema das neves <131496781+MusgoNato@users.noreply.github.com>
2024-10-11 13:38:05 -0400

    Add Alocacao Dinamica

--- funcoes.h
+++ funcoes.h
@@ -1,2 +1,13 @@
+typedef struct _entrada_user
+{
+    int id_funcao;
+    double x;
+    double precisao;
+    double n_raiz;
+}ENTRADA_USUARIO;
+
+/*Calcula seno*/
 double seno(double, int precisao);
+
+/*Calcula Fatorial*/
 int fat(int);
--- main.c
+++ main.c
@@ -13,7 +13,7 @@
 */
 
 # include <stdio.h>
-# include <math.h>
+# include <stdlib.h>
 # include "funcoes.h"
 
 /*PENDENTE:
@@ -21,19 +21,38 @@ VERIFICAR A IMPRESSAO DA PRECISAO E SE O CALCULO ESTA CORRETO MESMO!*/
 
 int main(int argc, char *argv[])
 {
-    double x;
-    int precisao;
+    ENTRADA_USUARIO **input;
+    int i = 0;
+    int cont_valores = 0;
 
     argc = argc;
     argv = argv;
 
-    printf("Insira o x: ");
-    scanf("%lf", &x);
-    printf("\nInsira a precisao: ");
-    scanf("%d", &precisao);
 
-    printf("MEU ------- : %f\n", seno(x, precisao));
-    printf("MATH.H ---- : %f", sin(x));
+    /*Alocacao para meu vetor de ponteiros*/
+    input = (ENTRADA_USUARIO **)malloc(sizeof(ENTRADA_USUARIO *));
+
+    printf("Insira os valores\n");
+    while(1)
+    {
+        /*Leitura da entrada padrao*/
+        fscanf(stdin, "%d %lf %lf %lf", &input[i]->id_funcao, &input[i]->x, &input[i]->precisao, &input[i]->n_raiz);
+        if(input[i]->id_funcao == 0)
+        {
+            break;
+        }
+        i++;
+        cont_valores++;
+
+        /*Alocacao para cada ponteiro*/
+        input[i] = (ENTRADA_USUARIO *)malloc(sizeof(ENTRADA_USUARIO));
+    }
+    
+    /*Impressao*/
+    for(i = 0; i < cont_valores; i++)
+    {
+        printf("%d linha: ID: |%d| X: |%f| PRECISAO: |%f| N_RAIZ: |%f|\n", i, input[i]->id_funcao, input[i]->x, input[i]->precisao, input[i]->n_raiz);
+    }
 
     return 0;
 }
